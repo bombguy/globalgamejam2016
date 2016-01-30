b120770a595d818ac08895567234a91a0560aae7
ingochris <ingochris@users.noreply.github.com>
2016-01-30 03:28:20 -0800

    Add collider components and orb destroy script

---
+++ Life of a Probe/Assets/Scripts/Orb.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Orb : MonoBehaviour {
+
+	Collider2D col;
+	// Use this for initialization
+	void Start () {
+		col = GetComponent<Collider2D> ();
+	}
+		
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter (Collision collision)
+	{
+		if(collision.gameObject.name == "Player")
+			
+		Debug.Log("Contact");
+		
+		Destroy (this);
+	}
+}
+
+
+	
+
+	
