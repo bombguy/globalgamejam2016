a5beaafc07251221ec10ee6bbc8d74e050915122
Isidro Gomez <gomez.isidro1@gmail.com>
2016-01-30 23:49:32 -0800

    recommit FlameAnimation.cs and LevelCount.cs

---
+++ Life of a Probe/Assets/Scripts/FlameAnimation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlameAnimation : MonoBehaviour {
+
+    public SpriteRenderer sr;
+    float cooldown = 0.0f;
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        sr.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!sr.enabled)
+        {
+            //sr.enabled = false;
+            cooldown += Time.deltaTime;
+            if (cooldown > 3)
+                sr.enabled = true;
+        }
+        else
+        {
+            cooldown -= Time.deltaTime;
+            if (cooldown < 0)
+            {
+                sr.enabled = false;
+                cooldown = 0;
+            }
+        }
+	}
+}
---
+++ Life of a Probe/Assets/Scripts/LevelCount.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelCount : MonoBehaviour {
+
+	// Use this for initialization
+    public Text textarea;
+    void Start()
+    {
+        textarea = GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        textarea.text = "   Current Level: " + GameInformation.level.ToString();
+    }
+}
